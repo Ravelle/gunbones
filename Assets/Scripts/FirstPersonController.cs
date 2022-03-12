a8997cf12af5e6c8227c618416a95a766bf67637
edvaudin <edward@vaudin.net>
2022-03-12 19:13:54 +0000

    Fixed merge conflict

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -73,24 +73,6 @@ namespace StarterAssets
         private Vector2 lookDirection;
         private const float lookThreshold = 0.01f;
 
-<<<<<<< Updated upstream
-		private void Awake()
-		{
-			if (mainCamera == null)
-			{
-				mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
-			}
-			playerController = new PlayerControls();
-			controller = GetComponent<CharacterController>();
-		}
-
-		private void Start()
-		{
-			// reset our timeouts on start
-			jumpTimeoutDelta = JumpTimeout;
-			fallTimeoutDelta = FallTimeout;
-		}
-=======
         // audio
         private RandomAudioPlayer randomAudioPlayer;
         [SerializeField] float walkingStepRate = 0.5f;
@@ -116,7 +98,6 @@ namespace StarterAssets
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
         }
->>>>>>> Stashed changes
 
         private void OnEnable()
         {
@@ -142,166 +123,6 @@ namespace StarterAssets
 
 
         private void Update()
-<<<<<<< Updated upstream
-		{
-			JumpAndGravity();
-			GroundedCheck();
-			Move();
-		}
-
-		private void LateUpdate()
-		{
-			CameraRotation();
-		}
-
-		private void GroundedCheck()
-		{
-			// set sphere position, with offset
-			Vector3 spherePosition = new Vector3(transform.position.x, transform.position.y - GroundedOffset, transform.position.z);
-			Grounded = Physics.CheckSphere(spherePosition, GroundedRadius, GroundLayers, QueryTriggerInteraction.Ignore);
-		}
-
-		private void CameraRotation()
-		{
-			lookDirection = look.ReadValue<Vector2>();
-			// if there is an input
-			if (lookDirection.sqrMagnitude >= lookThreshold)
-			{
-				cinemachineTargetPitch += lookDirection.y * RotationSpeed;
-				rotationVelocity = lookDirection.x * RotationSpeed;
-
-				// clamp our pitch rotation
-				cinemachineTargetPitch = ClampAngle(cinemachineTargetPitch, BottomClamp, TopClamp);
-
-				// Update Cinemachine camera target pitch
-				CinemachineCameraTarget.transform.localRotation = Quaternion.Euler(cinemachineTargetPitch, 0.0f, 0.0f);
-
-				// rotate the player left and right
-				transform.Rotate(Vector3.up * rotationVelocity);
-			}
-		}
-
-		private void Move()
-		{
-			moveDirection = move.ReadValue<Vector2>();
-
-			float targetSpeed = sprint.IsPressed() ? SprintSpeed : MoveSpeed;
-
-			if (moveDirection == Vector2.zero) targetSpeed = 0.0f;
-
-			float currentHorizontalSpeed = new Vector3(controller.velocity.x, 0.0f, controller.velocity.z).magnitude;
-
-			float speedOffset = 0.1f;
-
-			// accelerate or decelerate to target speed
-			if (currentHorizontalSpeed < targetSpeed - speedOffset || currentHorizontalSpeed > targetSpeed + speedOffset)
-			{
-				// creates curved result rather than a linear one giving a more organic speed change
-				// note T in Lerp is clamped, so we don't need to clamp our speed
-				//_speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude, Time.deltaTime * SpeedChangeRate);
-				speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed, Time.deltaTime * SpeedChangeRate);
-
-				// round speed to 3 decimal places
-				speed = Mathf.Round(speed * 1000f) / 1000f;
-			}
-			else
-			{
-				speed = targetSpeed;
-			}
-
-			Vector3 inputDirection = new Vector3(moveDirection.x, 0.0f, moveDirection.y).normalized;
-
-			// note: Vector2's != operator uses approximation so is not floating point error prone, and is cheaper than magnitude
-			// if there is a move input rotate player when the player is moving
-			if (moveDirection != Vector2.zero)
-			{
-				inputDirection = transform.right * moveDirection.x + transform.forward * moveDirection.y;
-			}
-
-			controller.Move(inputDirection.normalized * (speed * Time.deltaTime) + new Vector3(0.0f, verticalVelocity, 0.0f) * Time.deltaTime);
-		}
-
-		private void JumpAndGravity()
-		{
-			if (Grounded)
-			{
-				// reset the fall timeout timer
-				fallTimeoutDelta = FallTimeout;
-
-				// stop our velocity dropping infinitely when grounded
-				if (verticalVelocity < 0.0f)
-				{
-					verticalVelocity = -2f;
-				}
-
-				// Jump
-				if (jump.IsPressed() && jumpTimeoutDelta <= 0.0f)
-				{
-					// the square root of H * -2 * G = how much velocity needed to reach desired height
-					verticalVelocity = Mathf.Sqrt(JumpHeight * -2f * Gravity);
-				}
-
-				// jump timeout
-				if (jumpTimeoutDelta >= 0.0f)
-				{
-					jumpTimeoutDelta -= Time.deltaTime;
-				}
-			}
-			else
-			{
-				// reset the jump timeout timer
-				jumpTimeoutDelta = JumpTimeout;
-
-				// fall timeout
-				if (fallTimeoutDelta >= 0.0f)
-				{
-					fallTimeoutDelta -= Time.deltaTime;
-				}
-
-				// if we are not grounded, do not jump
-				//_input.jump = false;
-			}
-
-			// apply gravity over time if under terminal (multiply by delta time twice to linearly speed up over time)
-			if (verticalVelocity < terminalVelocity)
-			{
-				verticalVelocity += Gravity * Time.deltaTime;
-			}
-		}
-
-		private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
-		{
-			if (lfAngle < -360f) lfAngle += 360f;
-			if (lfAngle > 360f) lfAngle -= 360f;
-			return Mathf.Clamp(lfAngle, lfMin, lfMax);
-		}
-
-		private void DisableInputActions()
-		{
-			move.Disable();
-			look.Disable();
-			jump.Disable();
-			sprint.Disable();
-		}
-
-		private void OnDisable()
-		{
-			DisableInputActions();
-		}
-
-		private void OnDrawGizmosSelected()
-		{
-			Color transparentGreen = new Color(0.0f, 1.0f, 0.0f, 0.35f);
-			Color transparentRed = new Color(1.0f, 0.0f, 0.0f, 0.35f);
-
-			if (Grounded) Gizmos.color = transparentGreen;
-			else Gizmos.color = transparentRed;
-
-			// when selected, draw a gizmo in the position of, and matching radius of, the grounded collider
-			Gizmos.DrawSphere(new Vector3(transform.position.x, transform.position.y - GroundedOffset, transform.position.z), GroundedRadius);
-		}
-	}
-=======
         {
             JumpAndGravity();
             GroundedCheck();
@@ -479,5 +300,5 @@ namespace StarterAssets
             Gizmos.DrawSphere(new Vector3(transform.position.x, transform.position.y - GroundedOffset, transform.position.z), GroundedRadius);
         }
     }
->>>>>>> Stashed changes
+
 }
